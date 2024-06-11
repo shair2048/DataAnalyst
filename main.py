ea031b12f5662972b6186b10d16933bf8d5d4ad1
SHair <sonhai.it.2048@gmail.com>
2024-06-11 18:06:02 +0700

    xóa cmn aspas

--- main.py
+++ main.py
@@ -4,38 +4,8 @@ from streamlit_option_menu import option_menu
 from operation import uploadfile
 from pretreatment import information_page
 from models import choose_model
-from test import execute_model
-
-image_path = "ima.png"
-def get_base64_image(image_path):
-    with open(image_path, "rb") as image_file:
-        base64_image = base64.b64encode(image_file.read()).decode()
-    return base64_image
-base64_image = get_base64_image(image_path)
-st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background: url("data:image/jpg;base64,{base64_image}") no-repeat top right fixed;
-        background-size: 6%;
-        background-attachment: local;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 50px; /* Adjust the top margin here */
-    }}
-    .css-1v3fvcr {{
-        background-color: rgba(255, 255, 255, 0.8);
-        border-radius: 15px;
-        padding: 20px;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 def main():
-    
     global columns
     df = st.session_state.get('df')
     if df is None:
@@ -47,7 +17,7 @@ def main():
             st.experimental_rerun()
 
     with st.sidebar:
-        selected = option_menu("Data Analyst", ["File Information", 'Models', 'Model'], 
+        selected = option_menu("Data Analyst", ["File Information", 'Models'], 
                                icons=['file-earmark', 'bar-chart'], menu_icon="activity", default_index=0)
 
         
@@ -55,9 +25,6 @@ def main():
         information_page(df)
     elif selected == 'Models' and df is not None:
         choose_model(df)
-    elif selected == 'Model' and df is not None:
-        stored_df = st.session_state['df']
-        execute_model(stored_df)
 
 if __name__ == "__main__":
     main()
